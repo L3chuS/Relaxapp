5f224001958965804cdb2f8605fef8447253a400
“L3chuS” <eesantos.nqn@gmail.com>
2025-06-28 02:07:11 +0200

    Renamed app to app_instance

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import Interface.relaxapp as app
 
 if __name__ == "__main__":
     root = app.ctk.CTk()
-    app = app.RelaxApp_Structure(root)
+    app_instance = app.RelaxApp_Structure(root)
     root.mainloop()
